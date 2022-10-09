4e2d663566995e12e12274602ec4f2da2d1ec170
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-10-09 15:04:29 -0400

    added graphql queries for machines and items

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,9 +8,26 @@ const typeDefs = gql`
     email: String
   }
 
+  type Machine {
+    id: Int
+    model: String
+    revenue: Int
+    ownedBy: String
+  }
+
+  type Item {
+    id: Int
+    item_name: String
+    quantity: Int
+    costPerUnit: Int
+    insideOf: Int
+  }
+
   type Query {
     users: [User]
     user(username: String!): User
+    itemsForMachine(id: Int!): Item
+    machinesForUser(username: String!): Machine
   }
 
   type Mutation {
